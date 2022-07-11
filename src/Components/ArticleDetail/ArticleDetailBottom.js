f2aee6fae2ac00132aea7a7d067b5232bcb162b3
cebrail ilhan <cebovv65@gmail.com>
2022-07-12 01:27:05 +0300

    savepoint

--- src/Components/ArticleDetail/ArticleDetailBottom.js
+++ src/Components/ArticleDetail/ArticleDetailBottom.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./ArticleDetailBottom.scss";
 import twitterIcon from "../../Assets/Multimedia/twitter.svg";
 import facebookIcon from "../../Assets/Multimedia/facebook.svg";
+import eyesIcon from "../../Assets/Multimedia/eyes.svg";
 
 export default function ArticleDetailBottom() {
   return (
@@ -31,6 +32,18 @@ export default function ArticleDetailBottom() {
     <img src="https://images.unsplash.com/photo-1657491784312-cf194c7d08fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" 
     alt="" />
     <p>Mika Matikainen is a Design Founder & Advisor, Berlin School of Creative Leadership Executive MBA participant, Zippie advisor, Wolt co-founder, and Nordic Rose stakeholder.  </p>
+</section>
+
+<section className='articleDetailBottom__eyeLine'>
+    <div>
+    <img src={eyesIcon} alt="" />
+    </div>
+    
+</section>
+
+<section className='articleDetailBlogs'>
+
+    
 </section>
 
 
