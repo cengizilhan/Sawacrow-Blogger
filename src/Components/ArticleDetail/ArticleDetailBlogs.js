42b402beb9062359784b46c06d76a39975da8ec6
cebrail ilhan <cebovv65@gmail.com>
2022-07-18 20:56:57 +0300

    article detail bottom post

--- src/Components/ArticleDetail/ArticleDetailBlogs.js
+++ src/Components/ArticleDetail/ArticleDetailBlogs.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import ArticleDetailPost from './ArticleDetailPost'
+import './/ArticleDetailBlogs.scss'
 
 export default function ArticleDetailBlogs() {
   return (
-    <div><span></span></div>
+    <div className='ArticleDetailBlogs'><span className='ArticleDetailBlogs__title'>What to read next</span>
+    <div className='ArticleDetailBlogs__posts'>
+      <ArticleDetailPost></ArticleDetailPost>
+      <ArticleDetailPost></ArticleDetailPost>
+      <ArticleDetailPost></ArticleDetailPost>
+      </div></div>
   )
 }
