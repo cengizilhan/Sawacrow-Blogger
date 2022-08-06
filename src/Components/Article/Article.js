5ace237fbde960bd093c4bde521e2c791f00df13
cebrail ilhan <cebovv65@gmail.com>
2022-08-06 14:50:13 +0300

    axios get posts mainpage

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import {React, useEffect, useState }from "react";
 import './Article.scss'
+import axios from "axios";
 
 
-export default function Article() {
+
+export default function Article(props) {
+  const [imgUrl, setimgUrl] = useState([])
+
+
+  useEffect(() => {
+    
+
+
+    axios.get(`https://dummyblog.cengizilhan.com/wp-json/wp/v2/media/${props.featured_media}`).then(function (response) { // handle success
+    console.log(response.data.media_details.sizes.full.source_url);
+    setimgUrl(response.data.media_details.sizes.full.source_url);
+    
+    
+  });
+  
+
+  }, [])
+
+
+
   return (
     <section className='article__container'>
       <a href="/#">
+
+        
         <div className='article__container__image'>
             <img className='img-fluid' width="401" height="232" alt="bla bla"
-             src='https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' alt='article'></img>
+             src={imgUrl}></img>
         </div>
         <div className='article__container__text'>
-            <span>Here are some things you should know regarding how we work </span>
+            <span>{props.title.rendered}</span>
+            
             </div>
             </a>
     </section>
--- src/Pages/ArticleList.js
+++ src/Pages/ArticleList.js
@@ -1,21 +1,57 @@
-import React from "react";
+import {React, useEffect, useState }from "react";
 import ArticleHero from "../Components/ArticleHero/ArticleHero";
 import Article from "../Components/Article/Article";
 import "./ArticleList.scss";
+import axios from "axios";
 
 
 export default function ArticleList() {
+  const [postList, setpostList] = useState([])
+
+  useEffect(() => {
+    axios.get('https://dummyblog.cengizilhan.com/wp-json/wp/v2/posts')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      setpostList(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    }) 
+  }, [])
+
+
+
+
+  let article = {
+    title: "What to read next",
+    post: "lorem ipsum lorlaosdlao sdloq lwdoql doqwl",
+    img:"https://dummyblog.cengizilhan.com/wp-content/uploads/2022/08/wp_dummy_content_generator_1003-300x300.jpg"
+  }
+
+
+
   return (
     <section>
-  
+
       <section className="articleList">
         <ArticleHero></ArticleHero>
         <span className="articleList-header">All Articles</span>
-        <section className="articleList-container container">
-          <Article></Article>
-          <Article></Article>
-          <Article></Article>
-          <Article></Article>
+        <section className="articleList-container container">       
+
+{
+postList.map((x)=>(
+  <Article {...x}></Article>
+  
+  ))
+
+
+}
+
         </section>
       </section>
     
