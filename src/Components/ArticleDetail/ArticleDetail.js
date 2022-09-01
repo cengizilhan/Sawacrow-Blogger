f8ad6b8080a6e90d5524c03cd3ff4230f3185cb8
cebrail ilhan <cebovv65@gmail.com>
2022-09-01 22:12:22 +0300

    lazzy

--- src/Components/ArticleDetail/ArticleDetail.js
+++ src/Components/ArticleDetail/ArticleDetail.js
@@ -1,15 +1,14 @@
 import { React, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./ArticleDetail.scss";
-//import heroImg from "../../Assets/Multimedia/article-hero.jpg"; 
+//import heroImg from "../../Assets/Multimedia/article-hero.jpg";
 import twitterIcon from "../../Assets/Multimedia/twitter.svg";
 import facebookIcon from "../../Assets/Multimedia/facebook.svg";
 import ArticleDetailBottom from "./ArticleDetailBottom";
 import ArticleDetailBlogs from "./ArticleDetailBlogs";
 import { Helmet } from "react-helmet";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import PlaceholderSvg from '../../Assets/Multimedia/placeholder.svg';
-
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import PlaceholderSvg from "../../Assets/Multimedia/placeholder.svg";
 
 import axios from "axios";
 
@@ -19,9 +18,11 @@ export default function ArticleDetail(props) {
   const [imgUrl, setimgUrl] = useState([]);
   const [post, setpost] = useState([]);
   const [author, setauthor] = useState([]);
-
+  const [isLoading, setisLoading] = useState(true);
 
   useEffect(() => {
+    console.log("sawaaa");
+    console.warn(isLoading) 
     axios
       .get(
         "https://dummyblog.cengizilhan.com/wp-json/wp/v2/posts/" +
@@ -56,12 +57,15 @@ export default function ArticleDetail(props) {
      author açıklama */
         });
         // Fetch img url from another endpoint
-        axios.get(
+        axios
+          .get(
             `https://dummyblog.cengizilhan.com/wp-json/wp/v2/media/${response.data.featured_media}`
           )
           .then(function (response) {
             // handle success
-            setimgUrl({imgurl:response.data.media_details.sizes.full.source_url});
+            setimgUrl({
+              imgurl: response.data.media_details.sizes.full.source_url,
+            });
           });
 
         axios
@@ -85,69 +89,63 @@ export default function ArticleDetail(props) {
       })
       .then(function () {
         // always executed
+        setisLoading(false);
       });
   }, []);
+  console.log(isLoading)
+
+  // use effect içerisinde update oldukları için tekrar render ediyor. unlimited..
 
   return (
     <section className="articleDetail">
-    
-    {
-    //console.log("TİTLE", post.meta?.title)
-    }
+      {
+        //console.log("TİTLE", post.meta?.title)
+      }
       <Helmet>
         <title>{post.meta?.title}</title>
         <meta name="description" content={post.meta?.description} />
         <link rel="canonical" href={post.meta?.canonical} />
-        
-        {
-          imgUrl.imgurl && <link rel="preload" href={imgUrl.imgurl} as="image" />
-        }
+
+        {imgUrl.imgurl && (
+          <link rel="preload" href={imgUrl.imgurl} as="image" />
+        )}
       </Helmet>
       <h1 className="placeholder-glow">
-        <section className={post.title?'d-none':false}>
-        <span className="placeholder w-100"></span>
-        <span className="placeholder w-100"></span>
+        <section className={!isLoading ? "d-none" : false}>
+          <span className="placeholder w-100"></span>
+          <span className="placeholder w-100"></span>
         </section>
-        
+
         <span dangerouslySetInnerHTML={{ __html: post.title }}></span>
       </h1>
       <span className="articleDetail__subheader"></span>
-   
-    
-      <LazyLoadImage  wrapperClassName=" mx-auto d-block"
-              alt="herobanner" 
-              placeholderSrc={PlaceholderSvg}
-              
-              width = {400}
-              height = {400}
-              
-              className={imgUrl.imgurl?true:" placeholder "+" articleDetail__heroimg mx-auto"}
-              src={ imgUrl.imgurl}
-              
-           
-              
-        />
+
+
+      <LazyLoadImage
+        wrapperClassName=" mx-auto d-block"
+        alt="herobanner"
+        placeholderSrc={PlaceholderSvg}
+        width={400}
+        height={400}
+        className=   " articleDetail__heroimg mx-auto"
         
+        src={imgUrl.imgurl}
+      />
 
       <section className="articleDetail__wrapper container">
         <div className="articleDetail__hr"></div>
         <section className="articleDetail__author-container">
           <div className="articleDetail__author-left">
-          <LazyLoadImage  wrapperClassName="mx-auto d-block"
-              alt="author" 
+            <LazyLoadImage
+              wrapperClassName="mx-auto d-block"
+              alt="author"
               placeholderSrc={PlaceholderSvg}
-              
-              width = {57}
-              height = {57}
-              
+              width={57}
+              height={57}
               className="img-fluid"
               src={author.img}
-              
-           
-              
-        />
-            
-            
+            />
+
             <div>
               {" "}
               <span>{author.name}</span>
@@ -155,12 +153,22 @@ export default function ArticleDetail(props) {
             </div>
           </div>
           <div className="articleDetail__author-right">
-            <a target="_blank" href={'https://twitter.com/intent/tweet?url='+window.location.href}>
+            <a
+              target="_blank"
+              href={
+                "https://twitter.com/intent/tweet?url=" + window.location.href
+              }
+            >
               <div>
                 <img src={twitterIcon} alt="" />
               </div>
             </a>
-            <a target="_blank" href={'http://www.facebook.com/share.php?u='+window.location.href}>
+            <a
+              target="_blank"
+              href={
+                "http://www.facebook.com/share.php?u=" + window.location.href
+              }
+            >
               <div>
                 <img src={facebookIcon} alt="" />
               </div>
@@ -170,19 +178,24 @@ export default function ArticleDetail(props) {
       </section>
 
       <section className="articleDetail__content-wrapper container  placeholder-glow">
-        <section className={ post.content?'d-none':false } >
-        <span className="placeholder w-75"></span>
-        <span className="placeholder w-100"></span>
-        <span className="placeholder w-100"></span>
-        <span className="placeholder w-100"></span>
-        <span className="placeholder w-100"></span>
-        <span className="placeholder w-100"></span>
-        <span className="placeholder w-100"></span>
-        <span className="placeholder w-75"></span>
-        <span className="placeholder w-50"></span>
+        <section className={   !isLoading ? "d-none" : false}>
+          <span className="placeholder w-75"></span>
+          <span className="placeholder w-100"></span>
+          <span className="placeholder w-100"></span>
+          <span className="placeholder w-100"></span>
+          <span className="placeholder w-100"></span>
+          <span className="placeholder w-100"></span>
+          <span className="placeholder w-100"></span>
+          <span className="placeholder w-75"></span>
+          <span className="placeholder w-50"></span>
         </section>
-        {<div dangerouslySetInnerHTML={{ __html: post.content?post.content:''}}></div>}
-        
+        {
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post.content ? post.content : "",
+            }}
+          ></div>
+        }
       </section>
       <section className="articleDetail__wrapper container">
         <ArticleDetailBottom author={author}></ArticleDetailBottom>
